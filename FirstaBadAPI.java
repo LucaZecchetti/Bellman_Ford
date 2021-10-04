4d7cd999c0bff20f8fdf6189dd81a78b0f8a3229
Niyati Sinha <niyati.sinha29@gmail.com>
2021-10-04 20:15:41 +0530

    Create FirstaBadAPI.java

---
+++ FirstaBadAPI.java
@@ -0,0 +1,28 @@
+//278. First Bad Version
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+//Time Complexity: O(log n)
+//Space complexity: O(1)
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        int low=1;
+        int high=n;
+        int mid=1;
+        while(low<high)
+        {
+            mid=low+(high-low)/2;
+            if(!isBadVersion(mid))
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
+            }
+        }//first true is found when low==high
+        return low;
+    }
+}
